8c1e41cba000ed0fba5a426ca3ceee6076f012e9
Fabian Krüger <fkrueger@vmware.com>
2023-08-01 18:21:22 +0200

    Initial commit

---
+++ src/main/java/com/example/ghactionspg/GhactionspgApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghactionspg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhactionspgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhactionspgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ghactionspg/GhactionspgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghactionspg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhactionspgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
