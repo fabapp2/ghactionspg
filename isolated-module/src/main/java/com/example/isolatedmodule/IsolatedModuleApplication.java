ac652409d17ddb976403a8010380d03ec03af5cd
Fabian Krüger <fkrueger@vmware.com>
2023-08-02 11:48:50 +0200

    Add isolated module

---
+++ isolated-module/src/main/java/com/example/isolatedmodule/IsolatedModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.isolatedmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsolatedModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsolatedModuleApplication.class, args);
+    }
+
+}
